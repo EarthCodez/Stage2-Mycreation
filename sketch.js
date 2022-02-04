c0433a37de23e3f9b2171f29bdfb429c73ec03d1
EarthCodez_ <71922456+EarthCodez@users.noreply.github.com>
2022-02-04 08:32:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,75 @@
-var Player1,Player1s;
-var Player2,Player2s;
-var bg,bgs,bgs2;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
 var database;
-function preload() {
-    Player1 = loadAnimation("Images/Sprites/Blue/B1.png", "Images/Sprites/Blue/B2.png", "Images/Sprites/Blue/B3.png", "Images/Sprites/Blue/B4.png", "Images/Sprites/Blue/B5.png");
-    Player2 = loadAnimation("Images/Sprites/Red/R1.png", "Images/Sprites/Red/R2.png", "Images/Sprites/Red/R3.png", "Images/Sprites/Red/R4.png", "Images/Sprites/Red/R5.png");
-  bg=loadImage("Images/Bg/Morning.jpg");
+
+var form, player, game;
+
+var players, player1, player2;
+
+var player1_img , player2_img,bg1,player1_imgs,player2_imgs;
+var count=1;
+
+var flag,fs,fs2;
+var bg,bgs,bgs2;
+function preload(){
+  player1_img = loadAnimation("Images/Sprites/Blue/B1.png", "Images/Sprites/Blue/B2.png", "Images/Sprites/Blue/B3.png", "Images/Sprites/Blue/B4.png", "Images/Sprites/Blue/B5.png");
+  player2_img = loadAnimation("Images/Sprites/Red/R1.png", "Images/Sprites/Red/R2.png", "Images/Sprites/Red/R3.png", "Images/Sprites/Red/R4.png", "Images/Sprites/Red/R5.png");
+  bg=loadImage("Images/Bg/Morning.jpg")
+  bg1=loadImage("./images/form_bg.jpg")
+  flag=loadImage("Images/Sprites/flag.png")
 }
 
 function setup(){
-createCanvas(windowWidth,windowHeight-2);
- 
+  canvas = createCanvas(windowWidth-17.5, windowHeight-20);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 
-database = firebase.database();
 
-bgs=createSprite(0,150,windowWidth,windowHeight)
-bgs.addImage(bg)
-bgs.scale=1
+  bgs=createSprite(10000,500,windowWidth,windowHeight)
+  bgs.addImage(bg)
+  bgs.scale=1
+  
+  bgs2=createSprite(10000,800,windowWidth,windowHeight)
+  bgs2.addImage(bg)
+  bgs2.scale=1
 
-bgs2=createSprite(0,450,windowWidth,windowHeight)
-bgs2.addImage(bg)
-bgs2.scale=1
+ fs=createSprite(14300,500)
+ fs.addImage(flag)
+fs.scale=0.5
 
-Player1s= createSprite(200,200,200,200);
- Player1s.addAnimation("bluePlayer",Player1)
-
- Player2s=createSprite(200,500,200,200);
- Player2s.addAnimation("redPlayer",Player2)
+fs2=createSprite(14300,800)
+ fs2.addImage(flag)
+fs2.scale=0.5
 }
 
+
 function draw(){
-    background("red")
-    
-    drawSprites()
+  // background("red")
 
-    bgs.velocityX=-2;
-    bgs2.velocityX=-2;
+  // bgs.velocityX=-2;
+  //   bgs2.velocityX=-2;
 
-    if(bgs.x<-1000){
-        bgs.x=0
-    }
-    if(bgs2.x<-1000){
-        bgs2.x=0
-    }
+  //   if(bgs.x<1000){
+  //     bgs.x=0
+  // }
+  // if(bgs2.x<1000){
+  //     bgs2.x=0
+  // }
+
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
 }
